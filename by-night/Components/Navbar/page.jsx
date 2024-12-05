bb255360fc729733b77dc8ace2175224351e0eac
test <100705478+Wheelee@users.noreply.github.com>
2024-12-05 19:39:17 +0100

    Navbar

---
+++ by-night/Components/Navbar/page.jsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+        <nav className="flex flex-row w-full">
+            <div>
+                <h1 className="font-black">LOGO</h1>
+            </div>
+        </nav>
+    )
+}
